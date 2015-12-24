daef300d07ba487c60fa6e15e4fd03ab1dec4fc4
zethra <jediben97@gmail.com>
2015-12-24 18:23:57 -0500

    Added init script

--- install-service.sh
+++ install-service.sh
@@ -19,10 +19,14 @@ if [ -e /opt/wifiLights ]; then
     fi
 else
     mkdir /opt/wifiLights
+    mkdir /opt/wifiLights/bin
 fi
 
-mv bin/WifiLights /opt/wifiLights/wifiLights
+mv bin/WifiLights /opt/wifiLights/bin/wifiLights
 cp -r src/github.com/zethra/WifiLights/web /opt/wifiLights/
+
+mv src/github.com/zethra/WifiLights/wifiLights.sh /etc/init.d/wifiLights
+update-rc.d wifiLights defaults
 
 cd ..
 if [ -e temp ]; then
